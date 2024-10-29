6b07b6b7c63857da94f739cf1ea3f77cb9e68d63
Victor <Vi-69@yandex.ru>
2024-10-29 23:37:02 +0300

    feat: add typeTikTok

---
+++ src/socialMediaMethods/TikTok/types/typeTikTok.ts
@@ -0,0 +1,30 @@
+export interface TikTokResponse {
+  data: {
+    id: string;
+    title: string;
+    play: string;
+    wmplay: string;
+    hdplay: string;
+    music: string;
+    music_info: {
+      title: string;
+      play: string;
+      author: string;
+      original: boolean;
+      duration: number;
+      album: string;
+    };
+    play_count: number;
+    digg_count: number;
+    comment_count: number;
+    share_count: number;
+    download_count: number;
+    create_time: number;
+    author: {
+      id: string;
+      unique_id: string;
+      nickname: string;
+      avatar: string;
+    };
+  };
+}
