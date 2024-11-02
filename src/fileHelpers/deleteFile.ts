4a069eddf2ef017054418354cfbb5d357c35b526
Victor <Vi-69@yandex.ru>
2024-11-02 21:35:57 +0300

    refactor: add deleteFile

--- src/fileHelpers/deleteFile.ts
+++ src/fileHelpers/deleteFile.ts
@@ -1,18 +1,17 @@
 import * as fs from 'fs';
+import { Logger } from "../utils/Logger";
 
 /** Метод удаляет файл.
  @param filePath - путь к файлу который будет удален. */
 export async function deleteFile(filePath: string): Promise<void> {
   try {
-    console.log(`Попытка удаления файла: ${filePath}`);
-
     // Проверяем существует ли файл
     if (fs.existsSync(filePath)) {
       // Удаляем файл
       fs.unlinkSync(filePath);
-      console.log(`Файл успешно удален: ${filePath}`);
+      Logger.log(`Файл успешно удален: ${filePath}`);
     } else {
-      console.log(`Файл не найден по пути: ${filePath}`);
+      Logger.red(`При удаление файл не найден по пути: ${filePath}`);
     }
   } catch (error) {
     console.error(`Ошибка при удалении файла: ${error}`);
