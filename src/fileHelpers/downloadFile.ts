16d668697d967040a2997bdfcabef5faaf9ff00e
Victor <Vi-69@yandex.ru>
2024-11-02 21:35:37 +0300

    refactor: change downloadFile

--- src/fileHelpers/downloadFile.ts
+++ src/fileHelpers/downloadFile.ts
@@ -1,33 +1,26 @@
 import * as fs from 'fs';
 import axios from 'axios';
 import * as path from 'path';
+import { checkExistDownloadDirectory } from "./checkExistDownloadDirectory";
 
 /** Метод скачивает файл по ссылке и сохраняет в папку "downloads".
  @param url - ссылка на файл, который нужно скачать.
  @return string - путь к сохраненному файлу. */
 export async function downloadFile(url: string): Promise<string> {
-  // Проверяем расширение файла
-  if (url.toLowerCase().endsWith('.mp3')) {
-    console.log('Обнаружена попытка скачать mp3 файл. Операция прервана.');
-    throw new Error('Скачивание mp3 файлов запрещено');
-  }
-
   try {
     console.log('Начинаем загрузку файла...');
     console.log(`URL файла: ${url}`);
 
-    // Создаем папку downloads если она не существует
-    const downloadDir = path.join(process.cwd(), './downloads');
-    if (!fs.existsSync(downloadDir)) {
-      console.log('Папка downloads не найдена, создаем новую...');
-      fs.mkdirSync(downloadDir);
-      console.log(`Папка создана по пути: ${downloadDir}`);
-    } else {
-      console.log('Папка downloads уже существует');
-    }
+    // Получаем папку для сохранений
+    const downloadDir = checkExistDownloadDirectory()
 
-    // Генерируем уникальное имя файла используя текущую метку времени
-    const filename = `./temp_${Date.now()}.mp4`;
+    // Определяем тип и расширение файла из URL
+    const isAudio = url.toLowerCase().endsWith('.mp3');
+    const fileExtension = isAudio ? '.mp3' : '.mp4';
+    const prefix = isAudio ? 'Audio' : 'Video';
+
+    // Генерируем уникальное имя файла
+    const filename = `./${prefix}_temp_${Date.now()}${fileExtension}`;
     const filepath = path.join(downloadDir, filename);
     console.log(`Файл будет сохранен как: ${filepath}`);
 
@@ -44,11 +37,13 @@ export async function downloadFile(url: string): Promise<string> {
     const writer = fs.createWriteStream(filepath);
     response.data.pipe(writer);
 
-    // Возвращаем промис, который разрешится когда файл будет полностью записан
+    // Возвращаем promise, который разрешится когда файл будет полностью записан
     return new Promise((resolve, reject) => {
       writer.on('finish', () => {
-        console.log('Файл успешно загружен и сохранен');
-        console.log(`Полный путь к файлу: ${filepath}`);
+        console.log('Файл успешно загружен');
+        const stats = fs.statSync(filepath);
+        const fileSizeInMB = (stats.size / (1024 * 1024)).toFixed(2);
+        console.log(`Итоговый размер файла: ${fileSizeInMB} MB`);
         resolve(filepath);
       });
       writer.on('error', (error) => {
