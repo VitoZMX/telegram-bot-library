9fd20cf675af181fc5fe2f344e8dd60f7c8cd988
Victor <Vi-69@yandex.ru>
2024-11-17 20:53:47 +0300

    feat: add steamData

---
+++ src/socialMediaMethods/steam/data/steamData.ts
@@ -0,0 +1,18 @@
+// Список id игр по которым всегда будут собираться данные
+export const knownGameIds: number[] = [
+  730,  // Counter-Strike 2
+  2073850, // THE_FINALS
+  2357570, // Overwatch 2
+  578080, // PUBG
+  1172620, //Sea of Thieves
+  271590, // Grand Theft Auto V
+  526870, // Satisfactory
+  440, // Team Fortress 2
+  1422450, // Deadlock
+  1938090, // Call of Duty
+  570, // Dota 2
+  1172470, // Apex Legends
+  252490, // Rust
+  230410, // Warframe
+  739630  // Phasmophobia
+];
