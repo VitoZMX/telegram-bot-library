46991709587567376e209f0cd485294d3a8fc631
Victor <Vi-69@yandex.ru>
2024-12-15 17:48:10 +0300

    refactor: change types quill bot

--- src/bots/zmx_quill_bot/types/ZMXQuillBotType.ts
+++ src/bots/zmx_quill_bot/types/ZMXQuillBotType.ts
@@ -7,3 +7,10 @@ export interface SessionData {
 export interface MyContext extends Context {
   session: SessionData;
 }
+
+export interface MediaItem {
+  type: 'photo' | 'video';
+  media: string,
+  caption?: string,
+  parse_mode?: string
+}
