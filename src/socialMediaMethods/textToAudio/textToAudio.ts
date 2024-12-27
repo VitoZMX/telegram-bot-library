37c92918d948e767ee6c4b8e9619105ef4a13ead
Victor <Vi-69@yandex.ru>
2024-12-28 00:01:13 +0300

    feat: add textToAudioVoiceBuffer (draft)

---
+++ src/socialMediaMethods/textToAudio/textToAudio.ts
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { Logger } from "../../utils/Logger";
+
+require('dotenv').config({ path: '.env.tokens' });
+
+/** Метод перевода текста в аудио */
+export async function textToAudioVoiceBuffer(
+  text: string
+): Promise<Buffer> {
+  try {
+    const voiceId = process.env.ELEVENLABS_VOICE_ID;
+    if (!voiceId) throw new Error('ELEVENLABS_VOICE_ID не задан');
+
+    const apiKey = process.env.ELEVENLABS_API_KEY_ID;
+    if (!apiKey) throw new Error('ELEVENLABS_API_KEY_ID не задан');
+
+    Logger.log('Обработка текста в аудио на ElevenLabs...')
+    const response = await axios({
+      method: 'post',
+      url: `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream`,
+      headers: {
+        'Accept': '.mp3',
+        'xi-api-key': apiKey,
+        'Content-Type': 'application/json'
+      },
+      data: {
+        text,
+        model_id: 'eleven_multilingual_v2',
+        voice_settings: {
+          stability: 0.9,
+          similarity_boost: 0.9
+        }
+      },
+      responseType: 'arraybuffer'
+    });
+
+    if (!response.data) {
+      throw new Error('ElevenLabs не вернул аудио данные.');
+    }
+
+    Logger.log('Успешно...')
+    return Buffer.from(response.data);
+  } catch (error) {
+    Logger.red('Ошибка при запросе к ElevenLabs');
+    console.log(error);
+
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 401) {
+        throw new Error('Неверный API ключ');
+      }
+      if (error.response?.status === 422) {
+        throw new Error('Неверные параметры запроса');
+      }
+      throw new Error(`Ошибка API ElevenLabs: ${error.response?.status} ${error.response?.statusText}`);
+    }
+    throw new Error('Не удалось преобразовать текст в речь.');
+  }
+}
