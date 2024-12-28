53f8b17e0ec7fdfbb92b3d365960f17a1fcd0d4c
Victor <Vi-69@yandex.ru>
2024-12-28 15:03:07 +0300

    refactor: change model_id

--- src/socialMediaMethods/textToAudio/textToAudio.ts
+++ src/socialMediaMethods/textToAudio/textToAudio.ts
@@ -25,7 +25,7 @@ export async function textToAudioVoiceBuffer(
       },
       data: {
         text,
-        model_id: 'eleven_multilingual_v2',
+        model_id: 'eleven_flash_v2_5',
         voice_settings: {
           stability: 0.9,
           similarity_boost: 0.9
