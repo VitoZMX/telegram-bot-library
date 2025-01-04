1184d922093a15b08c3e40d636067b9f185f27ea
Victor <Vi-69@yandex.ru>
2025-01-04 12:55:56 +0300

    refactor: change names process

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -1,14 +1,14 @@
 const config = {
   apps: [
     {
-      name: "bot_caretaker",
+      name: "TG_caretaker",
       script: "./build/src/bots/zmx_caretaker_bot/zmx_caretaker_bot_v3.js",
       watch: ["./build/src"],
       ignore_watch: ["node_modules"],
       watch_delay: 1000,
     },
     {
-      name: "bot_quill",
+      name: "TG_quill",
       script: "./build/src/bots/zmx_quill_bot/zmx_quill_bot_v1.js",
       watch: ["./build/src"],
       ignore_watch: ["node_modules"],
